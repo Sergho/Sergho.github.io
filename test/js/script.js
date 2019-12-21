551da37bb3245b2cd487e5dc8aa820896daddcad
Sergho <52316559+Sergho@users.noreply.github.com>
2019-12-21 20:00:11 +0300

    Add files via upload

---
+++ test/js/script.js
@@ -0,0 +1,22 @@
+function DropdownToggle(){
+	// Кнопка меню (три полосочки)
+	const burger 		= document.querySelector("header.main .nav .dropdown-burger");
+	// Блок затемнения фона
+	const darkness 	= document.querySelector(".darkness");
+	// Само выпадающее меню
+	const menu			= document.querySelector("header.main .nav .dropdown-menu");
+
+	burger.addEventListener("click", () => {	// Открываем, показываем меню
+		darkness.style.display 	= "block";
+		menu.style.transform 		= "translate(0, 0)";
+		setTimeout(function(){darkness.style.opacity	= "1";}, 50);
+	});
+	darkness.addEventListener("click", () => {	// Закрываем меню
+		menu.style.transform = "translate(-100%, 0)";
+		darkness.style.opacity = "0";
+		setTimeout(function(){darkness.style.display = "none";}, 550);
+	});
+
+}
+
+DropdownToggle();
