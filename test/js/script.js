a1b8cc5dca104e2e364542e6e40e13c0ee08982d
Sergho <52316559+Sergho@users.noreply.github.com>
2019-12-28 22:19:20 +0300

    Add files via upload

--- test/js/script.js
+++ test/js/script.js
@@ -7,11 +7,13 @@ function DropdownToggle(){
 	const menu			= document.querySelector("header.main .nav .dropdown-menu");
 
 	burger.addEventListener("click", () => {	// Открываем, показываем меню
+		document.body.style.overflow = "hidden"; // Убираем прокрутку страницы
 		darkness.style.display 	= "block";
 		menu.style.transform 		= "translate(0, 0)";
 		setTimeout(function(){darkness.style.opacity	= "1";}, 50);
 	});
 	darkness.addEventListener("click", () => {	// Закрываем меню
+		document.body.style.overflow = "visible"; // Убираем прокрутку страницы
 		menu.style.transform = "translate(-100%, 0)";
 		darkness.style.opacity = "0";
 		setTimeout(function(){darkness.style.display = "none";}, 550);
