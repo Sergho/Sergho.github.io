8188426e93d6a4c10017f151b25077d00e5ad49d
Sergho <sergche04@gmail.com>
2020-01-03 19:26:42 +0300

    one more

--- test/js/script.js
+++ test/js/script.js
@@ -1,6 +1,4 @@
-/*	------- Открытие и закрытие выпадающего меню -------	*/
-
-function DropdownToggle(elem){
+function DropdownToggle(){
 	// Кнопка меню (три полосочки)
 	const burger 		= document.querySelector("header .nav .dropdown-burger");
 	// Блок затемнения фона
@@ -8,29 +6,30 @@ function DropdownToggle(elem){
 	// Само выпадающее меню
 	const menu			= document.querySelector("header .nav .dropdown-menu");
 
-	if(elem == burger){
-		// Открываем менюшку
+	burger.addEventListener("click", () => {	// Открываем, показываем меню
 		document.body.style.overflow = "hidden"; // Убираем прокрутку страницы
 		darkness.style.display 	= "block";
 		menu.style.transform 		= "translate(0, 0)";
 		setTimeout(function(){darkness.style.opacity	= "1";}, 50);
-	} else {
-		// Закрываем менюшку
+	});
+	darkness.addEventListener("click", () => {	// Закрываем меню
 		document.body.style.overflow = "visible"; // Убираем прокрутку страницы
 		menu.style.transform = "translate(-101%, 0)";
 		darkness.style.opacity = "0";
 		setTimeout(function(){darkness.style.display = "none";}, 550);
-	}
+	});
 
 }
-/*	--------------------- Прокрутка вверх ------------------	*/
+
 function ScrollTop(){
-	// Прокручиваем в начало
-	$('html, body').animate({scrollTop: 0}, 1000);
+	// Кнопка
+	const btn = document.querySelector(".scroll-top .chevron");
+
+	btn.addEventListener("click", function(){
+		// Прокручиваем в начало
+		$('html, body').animate({scrollTop: 0}, 1000);
+	});
 }
-
-/*	---- Открытие и закрытие модального окна авторизации ----	*/
-
 function Auth(){		// login
 	// Окно авторизации
 	const modal = document.querySelector("#auth");
@@ -41,18 +40,17 @@ function Auth(){		// login
 
 	if(darkness.style.display != "block"){
 		darkness.style.display = "block";
-		modal.style.display = "block";
 		setTimeout(function(){darkness.style.opacity = "1";}, 50);
 		setTimeout(function(){modal.style.opacity = "1";}, 50);
 	} else {
 		modal.style.opacity = "0";
 		darkness.style.opacity = "0";
 		setTimeout(function(){darkness.style.display = "none";}, 550);
-		setTimeout(function(){modal.style.display = "none";}, 550);
 	}
 
 }
 
+<<<<<<< HEAD
 /* --------------- Setting and removing a tick	--------------------- */
 
 function Tick(elem){
@@ -65,3 +63,7 @@ function Tick(elem){
 	else tick.style.opacity = "1";
 	console.log(chbox.checked);
 }
+=======
+DropdownToggle();
+ScrollTop();
+>>>>>>> parent of 7ca151b... end of main
