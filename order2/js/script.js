d98b915808c6f2caadf248d6f0d8776d7d2a9b5c
Sergho <52316559+Sergho@users.noreply.github.com>
2020-07-07 00:34:51 +0300

    order2 partners slider

--- order2/js/script.js
+++ order2/js/script.js
@@ -17,3 +17,59 @@ function DropdownClose(){
 	header.style.filter = "none";
 	setTimeout(() => {dropdown.style.display = "none"}, 550)
 }
+
+function VideoToggleMobile(button){
+	const video = button.parentNode.querySelector("video");
+
+	if(button.classList.contains("play")){
+		button.classList.remove("play");
+		button.classList.add("pause");
+		video.play();
+		video.setAttribute("control", "");
+	} else {
+		button.classList.add("play");
+		button.classList.remove("pause");
+		video.pause();
+		video.removeAttribute("control");
+	}
+}
+
+function PartnersPrev(button){
+	const root 	= button.parentElement;
+	const items = root.querySelectorAll(".item");
+
+	let first_index;
+
+	items.forEach((item, key) => {
+		if(item.style.transform == "translate(0%, 0px)") first_index = key;
+	});
+	
+	if(first_index != 0){
+		for(let i = first_index - 1; i < first_index + 6; i++){
+			let style = items[i].style.transform;
+			let pos		= +style.split("(")[1].split("%")[0];
+			pos += 100;
+			items[i].style.transform = "translate(" + pos + "%, 0)";
+		}
+	}
+}
+
+function PartnersNext(button){
+	const root 	= button.parentElement;
+	const items = root.querySelectorAll(".item");
+
+	let first_index;
+
+	items.forEach((item, key) => {
+		if(item.style.transform == "translate(0%, 0px)") first_index = key;
+	});
+	
+	if(first_index != items.length - 6){
+		for(let i = first_index; i < first_index + 7; i++){
+			let style = items[i].style.transform;
+			let pos		= +style.split("(")[1].split("%")[0];
+			pos -= 100;
+			items[i].style.transform = "translate(" + pos + "%, 0)";
+		}
+	}
+}
