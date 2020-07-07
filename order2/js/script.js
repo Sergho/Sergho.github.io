5a2b9ed10f747721f53f4c2266fd46e94e63215d
Sergho <52316559+Sergho@users.noreply.github.com>
2020-07-07 09:48:05 +0300

    order2 videos

--- order2/js/script.js
+++ order2/js/script.js
@@ -22,20 +22,108 @@ function DropdownClose(){
 	setTimeout(() => {dropdown.style.display = "none"}, 550)
 }
 
-function VideoToggleMobile(button){
+function VideoPlayMobile(button){
 	const video = button.parentNode.querySelector("video");
 
-	if(button.classList.contains("play")){
-		button.classList.remove("play");
-		button.classList.add("pause");
-		video.play();
-		video.setAttribute("control", "");
-	} else {
-		button.classList.add("play");
-		button.classList.remove("pause");
-		video.pause();
-		video.removeAttribute("control");
+	button.style.opacity = "0";
+	video.play();
+	setTimeout(() => {button.style.display = "none";}, 550);
+}
+
+function VideoPauseMobile(video){
+	const button = video.parentNode.querySelector(".play");
+
+	button.style.display = "block";
+	video.pause();
+	setTimeout(() => {button.style.opacity = "1";}, 50);
+}
+
+function VideoPrev(){
+	const numbers = document.querySelectorAll("main .projects .videos-desktop .control .list .item");
+	const videos 	= document.querySelectorAll("main .projects .videos-desktop .other-videos .other-video");
+	const main_video = document.querySelector("main .projects .videos-desktop .main-video video");
+
+	let current_index;
+	numbers.forEach((number, key) => {
+		if(number.classList.contains("active")){
+			current_index = key;
+		}
+	});
+
+	if(current_index != 0){
+		videos.forEach((video) => {
+			const style = video.style.transform;
+			let pos 		= +style.split(", ")[1].split("%")[0];
+			pos += 120;
+			video.style.transform = "translate(0, " + pos + "%)";
+		});
+
+		numbers.forEach((number) => {
+			number.classList.remove("active");
+		});
+
+		current_index--;
+		numbers[current_index].classList.add("active");
+
+		main_video.style.width = main_video.offsetWidth + "px";
+
+		const newurl = videos[current_index].querySelector("video").getAttribute("src");
+		main_video.setAttribute("src", newurl);
+		setTimeout(() => {main_video.style.width = "auto"}, 3000);
 	}
+}
+
+function VideoNext(){
+	const numbers = document.querySelectorAll("main .projects .videos-desktop .control .list .item");
+	const videos 	= document.querySelectorAll("main .projects .videos-desktop .other-videos .other-video");
+	const main_video = document.querySelector("main .projects .videos-desktop .main-video video");
+
+	let current_index;
+	numbers.forEach((number, key) => {
+		if(number.classList.contains("active")){
+			current_index = key;
+		}
+	});
+
+	if(current_index != videos.length - 1){
+		videos.forEach((video) => {
+			const style = video.style.transform;
+			let pos 		= +style.split(", ")[1].split("%")[0];
+			pos -= 120;
+			video.style.transform = "translate(0, " + pos + "%)";
+		});
+
+		numbers.forEach((number) => {
+			number.classList.remove("active");
+		});
+
+		current_index++;
+		numbers[current_index].classList.add("active");
+
+		main_video.style.width = main_video.offsetWidth + "px";
+
+		const newurl = videos[current_index].querySelector("video").getAttribute("src");
+		main_video.setAttribute("src", newurl);
+		setTimeout(() => {main_video.style.width = "auto"}, 3000);
+	}
+}
+
+function VideoPlayDesktop(button){
+	const video = button.parentNode.querySelector("video");
+
+	button.style.opacity = "0";
+	video.play();
+	video.setAttribute("controls", "");
+	setTimeout(() => {button.style.display = "none";}, 550);
+}
+
+function VideoPauseDesktop(video){
+	const button = video.parentNode.querySelector(".play");
+
+	button.style.display = "block";
+	video.pause();
+	video.removeAttribute("controls");
+	setTimeout(() => {button.style.opacity = "1";}, 50);
 }
 
 function PartnersPrev(button){
@@ -85,7 +173,6 @@ function NewsStart(){
 		const primary = description.querySelector(".primary");
 		description.style.height = primary.offsetHeight + "px";
 		description.style.bottom = "-" + (primary.offsetHeight - 4) + "px";
-		console.log(description.style.height);
 	})
 }
 
@@ -93,8 +180,6 @@ function NewsHoverIn(item){
 	const description = item.querySelector(".description");
 	const primary			= item.querySelector(".primary");
 	const secondary 	= item.querySelector(".secondary");
-
-	console.log(secondary.offsetHeight);
 
 	description.style.height = primary.offsetHeight + secondary.offsetHeight + "px";
 	description.style.bottom = -primary.offsetHeight + "px" + 4;
