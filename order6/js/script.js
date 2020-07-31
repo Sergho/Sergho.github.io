02c820f2cb21cbcf1c8724f52603b79b8821438f
Sergho <52316559+Sergho@users.noreply.github.com>
2020-07-31 20:20:53 +0300

    order6 slider

--- order6/js/script.js
+++ order6/js/script.js
@@ -26,3 +26,41 @@ function CloseMenu(){
 		menu.style.display = "none";
 	}, 550)
 }
+function PrevSlide(){
+	const slides = document.querySelectorAll("#news .slider .slides .slide");
+	let current_index;
+
+	slides.forEach((slide, key) => {
+		if(slide.classList.contains("current")) current_index = key;
+	});
+	if(current_index != 0){
+		slides.forEach((slide, key) => {
+			let current_value = slide.style.transform;
+			current_value = current_value.split("(")[1];
+			current_value = +current_value.split("%")[0];
+			const new_value = current_value + 109.5;
+			slide.style.transform = "translate(" + new_value + "%, 0)";
+			slide.classList.remove("current");
+			if(key == current_index - 1) slide.classList.add("current");
+		});
+	}
+}
+function NextSlide(){
+	const slides = document.querySelectorAll("#news .slider .slides .slide");
+	let current_index;
+
+	slides.forEach((slide, key) => {
+		if(slide.classList.contains("current")) current_index = key;
+	});
+	if(current_index != slides.length - 3){
+		slides.forEach((slide, key) => {
+			let current_value = slide.style.transform;
+			current_value = current_value.split("(")[1];
+			current_value = +current_value.split("%")[0];
+			const new_value = current_value - 109.5;
+			slide.style.transform = "translate(" + new_value + "%, 0)";
+			slide.classList.remove("current");
+			if(key == current_index + 1) slide.classList.add("current");
+		});
+	}
+}
