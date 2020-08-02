76288a3cc62a8ed960f7334637edbb0339d973b3
Sergho <52316559+Sergho@users.noreply.github.com>
2020-08-02 13:20:31 +0300

    order7 dropdown

---
+++ order7/js/script.js
@@ -0,0 +1,28 @@
+function OpenMenu(){
+	const menu = document.querySelector("#dropdown");
+	const header = document.querySelector("header");
+	const main = document.querySelector("main");
+	const body = document.body;
+
+	menu.style.display = "block";
+	body.style.overflow = "hidden";
+	setTimeout(() => {
+		header.style.filter = "blur(5px)";
+		main.style.filter = "blur(5px)";
+		menu.style.opacity = "1";
+	}, 50);
+}
+function CloseMenu(){
+	const menu = document.querySelector("#dropdown");
+	const header = document.querySelector("header");
+	const main = document.querySelector("main");
+	const body = document.body;
+
+	menu.style.opacity = "0";
+	header.style.filter = "none";
+	main.style.filter = "none";
+	setTimeout(() => {
+		menu.style.display = "none";
+		body.style.overflow = "visible";
+	}, 550);
+}
