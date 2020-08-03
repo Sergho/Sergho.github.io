99d5c02387783a356776eb76c1796392ce8d20d0
Sergho <52316559+Sergho@users.noreply.github.com>
2020-08-03 09:57:40 +0300

    order7 dropdown form

--- order7/js/script.js
+++ order7/js/script.js
@@ -26,3 +26,31 @@ function CloseMenu(){
 		body.style.overflow = "visible";
 	}, 550);
 }
+function OpenForm(){
+	const menu = document.querySelector("#form");
+	const header = document.querySelector("header");
+	const main = document.querySelector("main");
+	const body = document.body;
+
+	menu.style.display = "block";
+	body.style.overflow = "hidden";
+	setTimeout(() => {
+		header.style.filter = "blur(5px)";
+		main.style.filter = "blur(5px)";
+		menu.style.opacity = "1";
+	}, 50);
+}
+function CloseForm(){
+	const menu = document.querySelector("#form");
+	const header = document.querySelector("header");
+	const main = document.querySelector("main");
+	const body = document.body;
+
+	menu.style.opacity = "0";
+	header.style.filter = "none";
+	main.style.filter = "none";
+	setTimeout(() => {
+		menu.style.display = "none";
+		body.style.overflow = "visible";
+	}, 550);
+}
